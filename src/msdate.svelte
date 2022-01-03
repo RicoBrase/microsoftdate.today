01ae679465437bdebd3be442b9ce9588f880b1d8
Rico Brase <kontakt@rico-brase.de>
2022-01-03 12:50:40 +0100

    Initial commit

---
+++ src/msdate.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+    const beginOfEra = new Date(2021, 11, 31);
+    const todayDate = new Date();
+
+    const dateDiffInDays = Math.floor(
+        (todayDate.getTime() - beginOfEra.getTime())
+        / 1000 // convert to seconds
+        / 60 // convert to minutes
+        / 60 // convert to hours
+        / 24 // convert to days 
+        );
+
+    let msDateDay = beginOfEra.getDate() + dateDiffInDays;
+    let msDateMonth = 12;
+
+    while(msDateDay > 99) { 
+        msDateDay -= 99;
+        msDateMonth += 1;
+    }
+    
+    /* Since Javascript dates automatically change months, years, etc.
+     * if the date gets too big for the current month (e.g. 2021-12-32 <=> 2022-01-01),
+     * we need to write a custom formatting function.
+     * 
+     * Here, we utilize the native formatting to format the date according to the users locale,
+     * without being limited by the aforementionend wrapping in the Date objects. */
+    function formatDateWithDefaultBrowserLocale(year: number, month: number, day: number): string {
+        const dateToGetPatternFrom = new Date("2022-01-01");
+        const localeParts = (new Intl.DateTimeFormat().formatToParts(dateToGetPatternFrom));
+        return localeParts.map(({type, value}) => {
+            switch(type) {
+                case 'year':
+                    const yearStr = year.toString();
+                    return yearStr.substring(yearStr.length - value.length, value.length);
+                case "month":
+                    if(isNaN(parseFloat(value))) return convertNumberToStringWithLeadingZeroIfNeccessary(month);
+                    if(value.length === 1) return month;
+
+                    return convertNumberToStringWithLeadingZeroIfNeccessary(month);
+                case "day":
+                    if(isNaN(parseFloat(value))) return convertNumberToStringWithLeadingZeroIfNeccessary(day);
+                    if(value.length === 1) return day;
+
+                    return convertNumberToStringWithLeadingZeroIfNeccessary(day);
+                case "literal":
+                    return value;
+                default: return "";
+            }
+        }).join("");
+    }
+
+    /* Sometimes, we need to display dates with leading zeros. */
+    function convertNumberToStringWithLeadingZeroIfNeccessary(input: number): string {
+        return `${input < 10 && input > 0 ? "0" : ""}${input}`;
+    }
+
+    const formattedMsDate = formatDateWithDefaultBrowserLocale(2021, msDateMonth, msDateDay);
+
+</script>
+
+{formattedMsDate}
---
+++ src/routes/index.svelte
@@ -0,0 +1,87 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import Msdate from "../msdate.svelte";
+
+    let date: string | undefined;
+
+    onMount(() => {
+        date = document.getElementById("date").textContent;
+    });
+</script>
+
+<style>
+    :root {
+        font-size: 2vmax;
+    }
+
+    * {
+        font-family: "Oswald", sans-serif;
+        color: white;
+    }
+
+    main {
+        text-align: center;
+    }
+
+    div.separator {
+        display: block;
+        width: 80%;
+        margin: 0.5rem auto;
+        height: calc(1rem / 8);
+        background-color: #a5b1c2;
+    }
+
+    footer {
+        opacity: 0.6;
+        transition: opacity 0.5s ease-in-out;
+        text-align: center;
+        margin-top: 2rem;
+    }
+
+    footer:hover {
+        opacity: 1;
+    }
+
+    h3 {
+        font-weight: 300;
+        font-size: 2rem;
+        padding: 0;
+        margin: 0;
+    }
+
+    h1 {
+        font-weight: 700;
+        font-size: 3rem;
+        padding: 0;
+        margin: 0;
+    }
+
+    a, a:focus, a:visited {
+        color: #fd9644;
+    }
+</style>
+
+<svelte:head>
+    <title>{date ? `${date} | ` : ""}Get your current date in Microsoft Exchange era</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;700&display=swap" rel="stylesheet"/> 
+</svelte:head>
+ 
+<main>
+    <h3>Hello, today's date is</h3>
+    <h1 id="date"><Msdate></Msdate></h1>
+    <h3>according to the Microsoft Exchange era.</h3>
+
+    <!-- Wayback Machine link, in case the support page gets updated. -->
+    <!-- https://web.archive.org/web/20220102235702/https://techcommunity.microsoft.com/t5/exchange-team-blog/email-stuck-in-exchange-on-premises-transport-queues/ba-p/3049447 -->
+
+    <footer>
+        This obvious joke originates from a <a href="https://techcommunity.microsoft.com/t5/exchange-team-blog/email-stuck-in-exchange-on-premises-transport-queues/ba-p/3049447" rel="noopener nofollow" target="_blank">bugfix</a>, Microsoft introduced to fix the <a href="https://en.wikipedia.org/wiki/Time_formatting_and_storage_bugs#Year_2022" rel="noopener nofollow" target="_blank">Y2K22 bug</a>.
+
+        <div class="separator"></div>
+
+        <a href="https://github.com/RicoBrase/microsoftdate.today" rel="noopener nofollow" target="_blank">Find this project on GitHub</a>
+    </footer>
+</main>
+
